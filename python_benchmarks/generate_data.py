5ff29f9187b7469b05c8a32991a7390afce1b1de
alexliap <alexandrosliapates@gmail.com>
2025-06-26 21:23:20 +0300

    generate regression data script

--- python_benchmarks/generate_data.py
+++ python_benchmarks/generate_data.py
@@ -7,7 +7,30 @@ RANDOM_STATE = 0
 
 
 def make_simple_regression():
-    pass
+    x = np.array([i for i in range(100)])
+    y = 2 * x + 1
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x.astype(np.int32), y.astype(np.int32), test_size=0.2, shuffle=False
+    )
+
+    train = np.concat([x_train.reshape(-1, 1), y_train.reshape(-1, 1)], axis=1)
+
+    test = np.concat([x_test.reshape(-1, 1), y_test.reshape(-1, 1)], axis=1)
+
+    (
+        pl.DataFrame(train)
+        .rename(lambda column_name: "x_" + column_name[-1])
+        .rename({"x_1": "y"})
+        .write_csv("data/simple_train.csv")
+    )
+
+    (
+        pl.DataFrame(test)
+        .rename(lambda column_name: "x_" + column_name[-1])
+        .rename({"x_1": "y"})
+        .write_csv("data/simple_test.csv")
+    )
 
 
 def make_complex_regression():
@@ -27,13 +50,26 @@ def make_complex_regression():
         random_state=RANDOM_STATE,
     )
 
-    train = {"x": x_train, "y": y_train}
+    train = np.concat([x_train, y_train.reshape(-1, 1)], axis=1)
 
-    test = {"x": x_test, "y": y_test}
+    test = np.concat([x_test, y_test.reshape(-1, 1)], axis=1)
 
-    pl.DataFrame(train).write_csv("train.csv")
-    pl.DataFrame(test).write_csv("test.csv")
+    (
+        pl.DataFrame(train)
+        .rename(lambda column_name: "x_" + column_name[-1])
+        .rename({"x_6": "y"})
+        .write_csv("data/complex_train.csv")
+    )
+
+    (
+        pl.DataFrame(test)
+        .rename(lambda column_name: "x_" + column_name[-1])
+        .rename({"x_6": "y"})
+        .write_csv("data/complex_test.csv")
+    )
 
 
 if __name__ == "__main__":
+    make_simple_regression()
+
     make_complex_regression()
