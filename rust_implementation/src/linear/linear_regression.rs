f3285ad22954a7f1e6980670262a9638ee70e266
alexliap <alexandrosliapates@gmail.com>
2025-07-30 21:32:04 +0300

    chagned sklearn moder to proper sgd linear and beautify rust loop

---
+++ rust_implementation/src/linear/linear_regression.rs
@@ -0,0 +1,131 @@
+use ndarray::{Array, OwnedRepr};
+use ndarray::{Axis, concatenate};
+use polars::prelude::*;
+
+pub struct LinearRegression {
+    weights: Vec<f32>,
+}
+
+impl LinearRegression {
+    pub fn new() -> Self {
+        return Self {
+            weights: Vec::new(),
+        };
+    }
+
+    pub fn fit(
+        &mut self,
+        x: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+        y: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+        epochs: i32,
+        lr: f32,
+    ) {
+        self._init_weights(x.shape()[1] as i32);
+
+        let mut array_weights = Array::from_vec(self.weights.clone())
+            .into_shape_clone((self.weights.len(), 1))
+            .unwrap();
+
+        for epoch in 1..=epochs {
+            let grad = self._grad(x, y);
+
+            array_weights = array_weights - lr * grad;
+
+            self.weights = array_weights.flatten().to_vec();
+
+            let loss = self.loss(x, y);
+
+            print!(
+                "\nEpoch: {:?} | Loss: {:?} | Weights: {:?}\n",
+                epoch,
+                loss,
+                array_weights.flatten().to_vec()
+            )
+        }
+    }
+
+    fn _init_weights(&mut self, num_vars: i32) {
+        for _ in 1..=num_vars {
+            self.weights.push(0 as f32)
+        }
+        // bias weight
+        self.weights.push(0 as f32)
+    }
+
+    fn _grad(
+        &self,
+        x: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+        y: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+    ) -> ndarray::ArrayBase<OwnedRepr<f32>, ndarray::Dim<[usize; 2]>> {
+        let curr_preds = self.predict(x);
+
+        let cast_x = x.mapv(|x| x as f32);
+        let cast_y = y.mapv(|y| y as f32);
+
+        let grad = (1. / (x.shape()[0] as f32)) * cast_x.t().dot(&(curr_preds - cast_y));
+
+        return grad;
+    }
+
+    pub fn loss(
+        &self,
+        x: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+        y: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+    ) -> f32 {
+        let cast_y = y.mapv(|y| y as f32);
+        let error = self.predict(x) - cast_y;
+        let mean_squared_error = error.mapv(|error| error.powi(2)).mean().unwrap();
+
+        return mean_squared_error;
+    }
+
+    pub fn predict(
+        &self,
+        x: &ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+    ) -> ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 2]>> {
+        let array_weights = Array::from_vec(self.weights.clone())
+            .into_shape_clone((self.weights.len(), 1))
+            .unwrap();
+
+        let cast_x = x.mapv(|x| x as f32);
+
+        let x_bias = Array::from_elem((cast_x.shape()[0], 1), 1.);
+
+        let x_data = concatenate![Axis(1), cast_x, x_bias];
+
+        // print!("{x_data}");
+
+        let preds = x_data.dot(&array_weights);
+
+        // print!("\n\n{preds}\n\n");
+
+        return preds;
+    }
+
+    pub fn load_data(
+        &self,
+        path: &str,
+        target: &str,
+    ) -> (
+        ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+        ndarray::ArrayBase<ndarray::OwnedRepr<i64>, ndarray::Dim<[usize; 2]>>,
+    ) {
+        let file = std::fs::File::open(path).unwrap();
+
+        let df = CsvReader::new(file).finish().unwrap();
+
+        let x = df
+            .drop(target)
+            .unwrap()
+            .to_ndarray::<Int64Type>(IndexOrder::Fortran)
+            .unwrap();
+
+        let y = df
+            .select([target])
+            .unwrap()
+            .to_ndarray::<Int64Type>(IndexOrder::Fortran)
+            .unwrap();
+
+        return (x, y);
+    }
+}
--- rust_implementation/src/main.rs
+++ rust_implementation/src/main.rs
@@ -1,3 +1,25 @@
+mod linear;
+
 fn main() {
-    println!("Hello, world!");
+    let mut model = linear::linear_regression::LinearRegression::new();
+
+    let data_path = "../data/simple_train.csv";
+    let target_var = "y";
+
+    let (x, y) = model.load_data(data_path, target_var);
+
+    // print!("x: {} | y: {}", x, y);
+
+    // print!("x.shape: {:?} | y.shape: {:?}", x.shape()[0], y.shape()[1]);
+
+    // model.fit(&x, &y);
+
+    // for row in x {
+    //     print!("{row}")
+    // }
+
+    // let preds = model.predict(&x);
+
+    // print!("{preds}")
+    model.fit(&x, &y, 40, 0.0001);
 }
